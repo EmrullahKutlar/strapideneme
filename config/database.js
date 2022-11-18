74fca41b81d679459ccac45533b801cf51828477
EmrullahKutlar <emrullah.ktlr@gmail.com>
2022-11-18 21:16:44 +0300

    database config

--- config/database.js
+++ config/database.js
@@ -2,11 +2,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: env("DATABASE_HOST", "127.0.0.1"),
+      host: env("DATABASE_HOST", "strapi-database.cusz5n97dl8i.us-east-1.rds.amazonaws.com"),
       port: env.int("DATABASE_PORT", 5432),
       database: env("DATABASE_NAME", "strapi"),
-      user: env("DATABASE_USERNAME", ""),
-      password: env("DATABASE_PASSWORD", ""),
+      user: env("DATABASE_USERNAME", "postgres"),
+      password: env("DATABASE_PASSWORD", "367855249Ek."),
     },
     useNullAsDefault: true,
   },
