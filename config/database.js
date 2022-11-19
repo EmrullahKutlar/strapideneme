e7634fed3afefd277ea4ec2c7def3899890eaa95
EmrullahKutlar <emrullah.ktlr@gmail.com>
2022-11-19 16:22:10 +0300

    changing database config

--- config/database.js
+++ config/database.js
@@ -2,12 +2,27 @@ module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: env("DATABASE_HOST", "strapi-database.cusz5n97dl8i.us-east-1.rds.amazonaws.com"),
+      host: env(
+        "DATABASE_HOST",
+        "strapi-database.cusz5n97dl8i.us-east-1.rds.amazonaws.com"
+      ),
       port: env.int("DATABASE_PORT", 5432),
       database: env("DATABASE_NAME", "strapi"),
       user: env("DATABASE_USERNAME", "postgres"),
       password: env("DATABASE_PASSWORD", "367855249Ek."),
     },
     useNullAsDefault: true,
+    acquireConnectionTimeout: 1000000,
+    pool: {
+      min: 0,
+      max: 1,
+      acquireTimeoutMillis: 300000,
+      createTimeoutMillis: 300000,
+      destroyTimeoutMillis: 300000,
+      idleTimeoutMillis: 30000,
+      reapIntervalMillis: 1000,
+      createRetryIntervalMillis: 2000,
+    },
+    debug: false,
   },
 });
